f5044c53fcf953b9f55159186e3c69125b72fe84
feipengyang <feipeng.yang@outlook.com>
2022-01-01 15:23:18 -0600

    Leaflet Step 1 finished.

---
+++ Leaflet-Step-1/static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoiZmVpcGVuZyIsImEiOiJja3g2dGFpNWEydnFoMzBybmFqNXQ4aXR0In0.NbESpBeCEU8h86KDKLNPHA";
---
+++ Leaflet-Step-1/static/js/logic.js
@@ -0,0 +1,97 @@
+var myMap = L.map("map", {
+    center: [37.09, -95.71],
+    zoom: 5
+  });
+  
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/light-v10",
+    // id: "mapbox/streets-v11",mapbox://styles/mapbox/light-v10
+    accessToken: API_KEY
+}).addTo(myMap);
+
+
+// function that returns a color based on earthquakes  depth
+function getColor(d) {
+    return d > 90 ? '#d73027' :
+           d > 70  ? '#fc8d59' :
+           d > 50   ? '#fee08b' :
+           d > 30   ? '#d9ef8b' :
+           d > 10   ? '#91cf60' :
+                      '#1a9850';
+}
+
+
+//   get data from  USGS GeoJSON Feed page: https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson
+  
+var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+d3.json(url).then(function(response) {
+
+    // console.log(response.features);
+
+    // add each earthquake to the map with different size and color 
+    for (var i = 0; i < response.features.length; i++) {
+
+        var earthquake = response.features[i];
+    
+        if (earthquake) {
+            L.circleMarker(([earthquake.geometry.coordinates[1], earthquake.geometry.coordinates[0]]), {
+                fillOpacity: 0.8,
+                color: "black",
+                weight: 1,
+
+                // fill color is reflected by the depth of the earthquake
+                fillColor: getColor(earthquake.geometry.coordinates[2]),
+
+                // radius is reflected by the magnitude of the earthquake
+                radius: earthquake.properties.mag*5
+
+            }).addTo(myMap);
+        }
+      }
+
+    // Create a legend
+
+    var legend = L.control({position: 'bottomright'});
+
+    legend.onAdd = function (myMap) {
+
+        var div = L.DomUtil.create('div', 'info legend'),
+            grades = [0, 10, 30, 50, 70, 90],
+            labels = [];
+
+        // loop through our density intervals and generate a label with a colored square for each interval
+        for (var i = 0; i < grades.length; i++) {
+            div.innerHTML +=
+                '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+                grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+        }
+
+        return div;
+    };
+
+    legend.addTo(myMap);
+
+
+
+
+      
+});
+
+
+
+// Loop through the cities array and create one marker for each city object
+// for (var i = 0; i < cities.length; i++) {
+//     L.circleMarker(cities[i].location, {
+//       fillOpacity: 0.75,
+//       color: "white",
+//       fillColor: "purple",
+//       // Setting our circle's radius equal to the output of our markerSize function
+//       // This will make our marker's size proportionate to its population
+//       radius: markerSize(cities[i].population)
+//     }).bindPopup("<h1>" + cities[i].name + "</h1> <hr> <h3>Population: " + cities[i].population + "</h3>").addTo(myMap);
+//   }
